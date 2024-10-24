3751eda67444efcc4171c2d73e7c6cec3d2cf530
dhenson <davonhensontech@gmail.com>
2024-10-24 15:47:09 -0400

    Completed part 3 Yoon, Katie, Randi, Davon

--- ALAB-308-3-1.js
+++ ALAB-308-3-1.js
@@ -71,3 +71,55 @@ while (count > 1) {
     console.log(n, " is a prime number");
     count -= 1;
 } */
+console.log("===========================Part trios=========================");
+//======================Part trios===============================
+let data = "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26";
+// Loop through characters in string
+let cell = '';
+let cell1 = '';
+let cell2 = '';
+let cell3 = '';
+let cell4 = '';
+let cellNum = 1;
+
+// For each character
+for (let i = 0; i < data.length; i++) {
+    let char = data[i];
+
+    // If new word
+    if (char == "," || char == "\n") {
+        switch (cellNum) {
+            case 1:
+                cell1 = cell;
+                break;
+            case 2:
+                cell2 = cell;
+                break;
+            case 3:
+                cell3 = cell;
+                break;
+            case 4:
+                cell4 = cell;
+                break;
+        }
+        if (cellNum == 4){
+            cellNum = 1;
+        }
+        else {
+            cellNum += 1;
+        }
+        cell = "";
+    } else {
+        cell += char;
+    }
+
+    // If new line
+    if (char == "\n"){
+        console.log(cell1, cell2, cell3, cell4);
+        cell = "";
+        cell1 = '';
+        cell2 = '';
+        cell3 = '';
+        cell4 = '';
+    }
+}
